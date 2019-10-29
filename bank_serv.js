bb91bd0f9fcf29edb5ff9d094355ababa4cd836e
RollingHog <46373241+RollingHog@users.noreply.github.com>
2019-10-29 12:36:10 +0300

    added reading command from file and tables creation?

--- bank_serv.js
+++ bank_serv.js
@@ -2,32 +2,42 @@
 //card_id card_type acc_id
 //acc_id acc_type user_id
 
+const fs = require('fs');
 //npm install sqlite3
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('banking.db');
 
 try {
-  const creationreq = 
-  "CREATE TABLE IF NOT EXISTS users ("
-  + "user_id INT, "
-  + "nick TINYTEXT, "
-  + "full_name TINYTEXT, "
-  + "race TINYTEXT, "
-  + "guild INT, "
-  + "operator_id INT "
-  + ");"
-  + "CREATE TABLE IF NOT EXISTS accounts ("
-  + "acc_id INT, "
-  + "acc_type INT, "
-  + "user_id INT, "
-  + ");"
-  + "CREATE TABLE IF NOT EXISTS cards ("
-  + "card_id INT, "
-  + "card_type INT,"
-  + "acc_id INT "
-  + ");"
-  db.run(creationreq);
+  const creationreqs = fs.readFileSync('bank_oncreate.sql', 'utf8')
+    .replace(/\-\-.*(\r\n|$)/g, '')
+    .replace(/\r\n\s+\r\n/g, '\r\n\r\n')
+    .replace(/\r\n([^\r])/g, '$1')
+    .split('\r\n');
+  log(creationreqs)
+    
+  db.serialize( function() {
+    for (i of creationreqs) {
+      // log(i)
+      (db.run(i))
+    }
+  })
 } catch(e) {
   warn('db already exists or smth', e)
+}
+
+function log(...e) {
+  console.log(...e)
+}
+
+function info(...e) {//34 - dark blue
+  console.log("\x1b[36mInfo: \x1b[37m", ...e)
+}
+
+function warn(...e) {
+  console.warn("\x1b[33mWarning:\x1b[37m", ...e)
+}
+
+function error(...e) {
+  console.error("\x1b[31mError:\x1b[37m", ...e)
 }
